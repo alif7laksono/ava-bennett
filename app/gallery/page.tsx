44b2d57a9bea9151a9450824bd320cead5e498e5
Alif Laksono <85070659+alif7laksono@users.noreply.github.com>
2025-01-19 08:35:03 +0700

    add scroll-parallax

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -120,15 +120,12 @@ export default function Gallery() {
       {/* Modal */}
       {currentIndex !== null && (
         <div className="fixed inset-0 bg-black bg-opacity-90 flex justify-center items-center">
-          {/* Close button */}
           <button
             onClick={closeModal}
             className="absolute top-4 right-4 text-white text-xl p-2 bg-black bg-opacity-50 rounded-full z-50"
           >
             <X />
           </button>
-
-          {/* Navigation */}
           <button
             onClick={showPrevious}
             className="absolute left-4 text-white p-2 bg-black bg-opacity-50 rounded-full z-50"
---
+++ app/scroll/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { ParallaxScroll } from "@/components/ui/parallax-scroll";
+
+export default function ParallaxScrollDemo() {
+  return <ParallaxScroll images={images} />;
+}
+
+const images = [
+  "https://images.unsplash.com/photo-1733421797526-d64daa8e0249?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1736144596730-efdfd0022f36?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1537769060554-b41893a07504?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1508437769842-8fb53c0a6d24?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHZpZXd8ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1492457450593-6f836126e2f6?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHZpZXd8ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1428908728789-d2de25dbd4e2?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1454391304352-2bf4678b1a7a?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODh8fGJlYWNofGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1506126483163-f4d1558dbf85?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fGJlYWNofGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1523538290088-51e3e7d1c00d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fGJlYWNofGVufDB8fDB8fHww",
+  "https://images.unsplash.com/photo-1518352724948-729151797553?q=80&w=2792&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1603389016863-aa1ba5c457cb?q=80&w=2864&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1604751292196-edd4f0ab8621?q=80&w=2833&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+  "https://images.unsplash.com/photo-1736264335209-05960b7aa567?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1563362554-5b521e37aa2a?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1727548099918-fa8da52df671?q=80&w=2301&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+];
